3fb70e1e3138b974b6913cd6cc59496f049d25bb
apip <apip@gitlab.lab>
2020-06-14 00:52:51 +0700

    creating alert

--- rn-guess-number-app/screens/StartGameScreen.js
+++ rn-guess-number-app/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Button, View, Text, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { StyleSheet, Button, View, Text, TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Colors from "../constants/Colors";
@@ -20,7 +20,12 @@ const StartGameScreen = props => {
 
     const confirmInputHandler = () => {
         const choosenNumber = parseInt(enteredValue);
-        if (choosenNumber === NaN || choosenNumber <= 0 || choosenNumber > 99) {
+        if (isNaN(choosenNumber) || choosenNumber <= 0 || choosenNumber > 99) {
+            Alert.alert(
+                'Invalid number!',
+                'Number has to be a number between 1 and 99.',
+                [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+            );
             return;
         }
         setConfirmed(true);
