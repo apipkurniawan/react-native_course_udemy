175bbdf2a1174b78d2413c0c2c25efb0e87b2fda
apip <apip@gitlab.lab>
2020-06-14 07:06:14 +0700

    finish the confirmation box

--- rn-guess-number-app/screens/StartGameScreen.js
+++ rn-guess-number-app/screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { StyleSheet, Button, View, Text, TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 import Colors from "../constants/Colors";
 
 const StartGameScreen = props => {
@@ -31,11 +32,18 @@ const StartGameScreen = props => {
         setConfirmed(true);
         setSelectedNumber(choosenNumber);
         setEnteredValue('');
+        Keyboard.dismiss();
     }
 
     let confirmedOutput;
     if (confirmed) {
-        confirmedOutput = <Text>Choosen Number: {selectedNumber}</Text>
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="START GAME" />
+            </Card>
+        )
     }
 
     return (
@@ -98,6 +106,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center'
     }
 });
 
