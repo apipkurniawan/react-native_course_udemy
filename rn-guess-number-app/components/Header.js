35700e49a075275346e327a2acb1cfe292a21fdb
apip <apip@gitlab.lab>
2020-07-03 10:59:55 +0700

    Working with Platform.select() and Platform in "if" Checks

--- rn-guess-number-app/components/Header.js
+++ rn-guess-number-app/components/Header.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Platform } from 'react-native';
 import Colors from '../constants/Colors';
 
 const Header = props => {
     return (
-        <View style={styles.header}>
+        <View style={{
+            ...styles.headerBase,
+            ...Platform.select({
+                ios: styles.headerIos,
+                android: styles.headerAndroid,
+            })
+        }}>
             <Text style={styles.headerTitle}>{props.title}</Text>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         padding: 20,
-        backgroundColor: Colors.primary,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+    },
+    headerIos: {
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary,
+        borderBottomColor: 'transparent',
+        borderBottomWidth: 0
     },
     headerTitle: {
-        color: 'black',
+        color: 'white',
         fontSize: 22,
         fontFamily: 'open-sans',
         marginTop: 30
--- rn-guess-number-app/components/MainButton.js
+++ rn-guess-number-app/components/MainButton.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Platform,
+    TouchableNativeFeedback
+} from 'react-native';
 
 import Colors from "../constants/Colors";
 
 const MainButton = props => {
+    let ButtonComponent = TouchableOpacity;
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        ButtonComponent = TouchableNativeFeedback;
+    }
     return (
-        <TouchableOpacity activeOpacity={0.8} onPress={props.onClick}>
-            <View style={styles.btn}>
-                <Text style={styles.btnText}>{props.children}</Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.btnContainer}>
+            <ButtonComponent activeOpacity={0.8} onPress={props.onClick}>
+                <View style={styles.btn}>
+                    <Text style={styles.btnText}>{props.children}</Text>
+                </View>
+            </ButtonComponent>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    btnContainer: {
+        borderRadius: 20,
+        overflow: "hidden"
+    },
     btn: {
         backgroundColor: Colors.primary,
         paddingVertical: 12,
