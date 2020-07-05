4f8926f487379a54c7b7e69a48417f279a847154
apip <apip@gitlab.lab>
2020-07-05 14:34:10 +0700

    rendering a meals list

--- the-meals-app/screens/CategoryMealsScreen.js
+++ the-meals-app/screens/CategoryMealsScreen.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = props => {
   console.log('props ', props);
+  const catId = props.navigation.getParam('categoryId');
+
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  const renderMealItem = itemData => {
+    return <MealItem
+      title={itemData.item.title}
+      onSelectMeal={() => { }}
+      duration={itemData.item.duration}
+      complexity={itemData.item.complexity}
+      image={itemData.item.imageUrl}
+      affordability={itemData.item.affordability}
+    />
+  };
+
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen!</Text>
-      <Button title="Go To Details Meal" onPress={() => {
-        /* cara 1 */
-        // props.navigation.navigate({ routeName: 'MealDetail' });
-        /* cara 2 */
-        props.navigation.push('MealDetail');
-        /* cara 3 (hilangkan icon back di iphone) */
-        // props.navigation.replace('MealDetail');
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: '100%' }} />
+
+
+
+      {/* <Button title="Go To Details Meal" onPress={() => {
+        props.navigation.navigate({ routeName: 'MealDetail' }); // cara 1
+        props.navigation.push('MealDetail'); // cara 2 
+        props.navigation.replace('MealDetail'); // cara 3 (hilangkan icon back di iphone)
       }} />
       <Button title="Go Back" onPress={() => {
-        /* cara 1 */
-        // props.navigation.goBack();
-        /* cara 2 */
-        props.navigation.pop();
-      }} />
+        props.navigation.goBack(); // cara 1
+        props.navigation.pop(); // cara 2
+      }} /> */}
     </View>
   );
 };
